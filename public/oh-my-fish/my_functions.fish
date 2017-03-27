c2f4255216b26fed7e88e48c06e90faae7e54bad
chenxc <chenxc@stargoto.com>
2017-03-27 19:14:57 +0800

    完成vim初始化脚本，不用每次都苦逼配环境了

--- public/oh-my-fish/my_functions.fish
+++ public/oh-my-fish/my_functions.fish
@@ -3,6 +3,11 @@
 function brs
     hg branches | grep $argv
 end
+function _1920
+    sudo xrandr --newmode "1920x1080_60.00" 173.00  1920 2048 2248 2576  1080 1083 1088 1120 -hsync +vsync
+    sudo xrandr --addmode DP2 "1920x1080_60.00"
+    sudo xrandr --output DP2 --mode "1920x1080_60.00"
+end
 
 set -x POLYSH_DIR /home/cxc/workspace/dotfile/public/polysh
 set -x PATH /home/cxc/workspace/dotfile/public/polysh/shell $PATH
--- public/vim/init.sh
+++ public/vim/init.sh
@@ -1,41 +1,43 @@
 #/bin/bash
 # init vim envirment for new vim
 # detected autoload dir exists
+
 function install()
 {
     baseDir="/home/cxc/.vim/bundle/"
     name=$1
     gitUrl=$2
     if [ ! -d $baseDir$name ]; then
+        echo "开始安装插件 "{$name}
         cd $baseDir && git clone $gitUrl $name
         status=$? 
         if [ ! $status -eq 0 ]; then
-            echo "install "{$name}" error "{$status}
+            echo "安装 "{$name}" 失败 "{$status}
         fi
     else
-        echo "plugin "{$name}" already installed"
+        echo "插件 "{$name}" 已经安装了, 跳过.."
     fi
 }
 
 if [ ! -d ~/.vim/autoload ]; then
-    echo "create autoload dir"
+    echo "创建 autoload 文件夹"
     mkdir -p ~/.vim/autoload
 else
-    echo "autoload dir already exists"
+    echo "文件夹 autoload 已经存在了,跳过..."
 fi
 
 if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
-    echo "install pathogen"
+    echo "安装 pathogen"
     curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 else
-    echo "pathoten already installed"
+    echo "pathoten 已经安装了"
 fi
 
 if [ ! -d ~/.vim/bundle ]; then
-    echo "create bundle dir"
+    echo "创建 bundle 文件夹"
     mkdir -p ~/.vim/bundle
 else
-    echo "bundle dir already exists"
+    echo "文件夹 bundle 已经存在了, 跳过"
 fi
 
 install minibufexpl.vim git@github.com:fholgado/minibufexpl.vim.git
@@ -43,3 +45,25 @@ install ctrlp.vim https://github.com/kien/ctrlp.vim.git
 install nerdtree https://github.com/scrooloose/nerdtree.git
 install vim-easy-align https://github.com/junegunn/vim-easy-align.git
 install tagbar https://github.com/majutsushi/tagbar.git
+install DoxygenToolkit.vim git@github.com:vim-scripts/DoxygenToolkit.vim.git
+install easymotion git@github.com:easymotion/vim-easymotion.git
+install gitgutter git@github.com:airblade/vim-gitgutter.git
+#install airline git@github.com:vim-airline/vim-airline.git
+install powerline git@github.com:powerline/powerline.git
+
+if [ ! -f ~/.vimrc ]; then
+    echo "自动安装 ~/.vimrc"
+    cp vimrc ~/.vimrc
+else
+    echo "~/.vimrc 文件已经存在了"
+    currentMd5=`md5sum ~/.vimrc | awk '{print $1}'`
+    stableMd5=`md5sum ./vimrc | awk '{print $1}'`
+    if [ $currentMd5 == $stableMd5 ]; then
+        echo "~/.vimrc文件与版本库文件相同，跳过.."
+    else
+        echo "开始比较vimrc文件差异"
+        meld ~/.vimrc ./vimrc
+        echo "差异比较完毕"
+    fi
+fi
+echo "vim环境初始化完毕"
